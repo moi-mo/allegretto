c67239fe6d46aec4c8f36fe34492fc48fe894586
moi-mo <3066628847@qq.com>
2020-03-19 13:15:32 +0800

    bug fixed,v12.1

--- bangumi_recorder.py
+++ bangumi_recorder.py
@@ -1,4 +1,5 @@
 # 番剧进度记录 by moimo233
+# 锟斤拷问题暂时无解
 
 #规定每个番剧class对应4个变量：
 class Bangumi :
@@ -11,19 +12,19 @@ class Bangumi :
 # load
 # 格式为name episode time，中间用空格分开，不同的项目之间是\n
 def load():
-    f = open('data','r')
+    f = open('bangumi_recorder/data.data','r')
     s = f.read()
     f.close()
 
-    bangumi_list = s.split(str = '\n')
+    bangumi_list = s.split(sep = '\n')
     bangumi_detail_list = []
     for i in bangumi_list :
         i = i.split()
-        na = i[0]
-        ep = i[1]
-        ti = i[2]        
-        exec(na + '= Bangumi(na,ep,ti)')
-        bangumi_detail_list += na
+        if i != []:   # 列表要不为空才可以使用l[0]
+            na = i[0]
+            ep = i[1]
+            ti = i[2]
+            bangumi_detail_list.append(Bangumi(na,ep,ti))
     return bangumi_detail_list
 ###########################################
 
@@ -31,31 +32,39 @@ def load():
 # open(xxx,'a')，即在data文件后追加
 def new(bangumi):
     l = load()
-    if bangumi.name in l:
+    is_in = False
+    for i in l:
+        if i.name == bangumi.name:
+            is_in = True
+    if is_in:
         print('错误！这部番已经在列表里了哦？')
     else:
-        f = open('data','a')
-        f.write(bangumi.name,' ',bangumi.episode,' ',bangumi.time,'\n')
+        f = open('bangumi_recorder/data.data','a')
+        f.write(bangumi.name+' '+bangumi.episode+' '+bangumi.time+'\n')
         f.close()
         print('番剧',bangumi.name,'新建成功！')
 ###########################################
 
 # mark
 # 本质上是覆盖
-def mark(bangumi,change_type,new_val):
+def mark(name,change_type,new_val):
     l = load()
-    if bangumi.name not in l:
+    is_in = False
+    for i in l:
+        if i.name == name:
+            is_in = True
+            bangumi_index = l.index(i)
+    if not is_in:
         print('错误！本番剧还未新建！\n打错了吗？')
     else:
         # change_type和new_val都是str
-        exec('l[l.index(bangumi)].'+change_type+'=new_val')
+        exec('l[bangumi_index].'+change_type+'=new_val')
         s = ''
         for i in l:
-            s += i.name+' '+i.episode+' 'i.time+'\n'
-        f = open('data','w')
+            s += i.name+' '+i.episode+' '+i.time+'\n'
+        f = open('bangumi_recorder/data.data','w')
         f.write(s)
         f.close()
-        print('记录成功！')
 
 # check_all
 def check_all():
@@ -93,9 +102,15 @@ def version():
 #############以下是程序运行的main：
 
 print('Hi~喜欢今天看的番剧吗？\n开始记录吧w，如有疑问请查阅README.md或者输入help')
-s = input()
-l = s.split()
-order = l[0]
+
+def get_order():
+    global l,order
+    s = input('>>> ')
+    l = s.split()
+    order = l[0]
+
+get_order()
+
 while order != 'exit':
     if order == 'new':
         if len(l) == 2:     #new [name]
@@ -106,19 +121,24 @@ while order != 'exit':
             new(b)
         else:
             print('命令格式错误！\nnew [name]，或者\nnew [name] [episode] [time]')
+        get_order()
 
     elif order == 'mark':
         if len(l) == 4:     #mark [name] [change_type] [new_val]
             mark(l[1],l[2],l[3])
+            print('记录成功！')
         else:
             print('命令格式错误！\nmark [name] [change_type] [new_val]')
+        get_order()
 
     elif (order == 'br') or (order == 'BangumiRecorder'):
         if len(l) == 4:
             mark(l[1],'episode',l[2])
             mark(l[1],'time',l[3])
+            print('记录成功！')
         else:
-            print('命令格式错误！\nbr [name] [episode] [time]')
+            print('命令格式错误！\nbr [name] [episode] [time] (BangumiRecorder=br)')
+        get_order()
 
     elif order == 'check':
         if len(l) == 2 and l[1] == 'all':     #check all
@@ -127,6 +147,11 @@ while order != 'exit':
             check(l[1])
         else:
             print('命令格式错误！\ncheck all\n或check [name]')
-            
+        get_order()
+    
+    elif order == 'version':
+        version()
+        get_order()
+
 
 
